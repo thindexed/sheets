3dc00f8d0153b738ae0ea851e88d937d1479e7f3
Herz, Andreas <a.herz@freegroup.de>
2022-04-14 14:30:26 +0200

    .

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+# Settings in .env have prio
+if [ -f .tekton/env.ini ]; then
+  set -o allexport
+  source .tekton/env.ini
+  set +o allexport
+fi
+
+# Settings in .env have prio
+if [ -f ./configuration/settings.ini ]; then
+  set -o allexport
+  source ./configuration/settings.ini
+  set +o allexport
+fi
+
+
+VERSION=$(uuidgen)
+OCIIMAGE="$OCIIMAGE:$VERSION"
+
+echo "Docker Image: $OCIIMAGE"
+echo "Using Domain: $DOMAIN"
+
+
+cp -R ./data/ ./src/data/
+cd src
+
+
+echo
+echo 'Building new image'
+docker build --no-cache=true --rm -t $OCIIMAGE .
+
+echo
+echo 'Push new image'
+docker push $OCIIMAGE
+
+cd ..
+
+echo "Using Domain: $DOMAIN"
+
+cat ./yaml/deployment.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | sed "s~<OCIIMAGE>~$OCIIMAGE~g" | kubectl apply -f -
+cat ./yaml/istio-authpolicy.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
+cat ./yaml/istio-virtualservice.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
+cat ./yaml/service.yaml | sed "s~<DOMAIN>~$DOMAIN~g" | kubectl apply -f -
